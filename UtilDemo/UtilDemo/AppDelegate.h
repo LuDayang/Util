67c602177e18476b3753c62755d63079471e5fbb
卢达洋 <ludy@n2n2n.com>
2015-12-18 00:55:51 +0800

    创建工程

---
+++ UtilDemo/UtilDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UtilDemo
+//
+//  Created by Lu on 15/12/18.
+//  Copyright © 2015年 Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UtilDemo/UtilDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UtilDemo
+//
+//  Created by Lu on 15/12/18.
+//  Copyright © 2015年 Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
